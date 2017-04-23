8cf08b3539e9733b83d11cf58ccbae0aad652875
Tommy Thompson <tomthompson828@gmail.com>
2017-04-22 23:57:36 -0400

    process.envs for heroku

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # HackRUSpring2017
-Simple Twilio NodeJS Cron Job
+Creating a Simple Twilio NodeJS Cron Job. This can serve as a cool biolerplate.
+
+
+## Twilio Account
+This app needs a Twilio account and phone number tied to said account. Below is how to set that up:
+
+  1. Go to [Twillio](https://www.twilio.com/) and sign up for a account
+  2. Once your account is made, check your [Twilio Console](https://www.twilio.com/console) for your Account SID and Auth Token. Use these in your environmental variables (see below).
+  3. Then, add a [Twilio Phone Number](https://www.twilio.com/console/phone-numbers/incoming) to your account. Also use this in your environmental variables
+  4. If you have a free trial, you can [Verify Other Phone Numbers](https://www.twilio.com/console/phone-numbers/verified). Use this if you want to text other numbers with your account that are not the number you signed up with.
+
+
+## Environmental Variables
+For security, you should never push your API keys to github. This is how to set your environmental variables in Heroku. NOte this assuming that you already have the Heroku CLI.
+
+Set up the TWILLIO_SID: `$ heroku config:set TWILLIO_SID={your sid here}` 
+
+Set up the TWILLIO_TOKEN: `$ heroku config:set TWILLIO_TOKEN={your token here}` 
+
+Set up the TWILIO_NUMBER: `$ heroku config:set TWILLIO_NUMBER={your twilio phone number here}` 
+
+Set up the number to text: `$ heroku config:set TEXT_THIS_NUMBER={your real phone number here}`
---
+++ index.js
@@ -0,0 +1,41 @@
+// Import Node Modules
+var schedule = require('node-schedule');
+var twilio = require('twilio');
+
+// Enivronmental Variables for your Twilio Account
+var accountSid = process.env.TWILLIO_SID; // Environmental Variable of your Account SID from www.twilio.com/console
+var authToken = process.env.TWILLIO_TOKEN;  // Environmental Variable of your Auth Token from www.twilio.com/console
+var twilioNumber = process.env.TWILIO_NUMBER; // Environmental Variable of your Twilio Account Phone Number from https://www.twilio.com/console/phone-numbers/incoming
+
+// Add the number you want to text below:
+var textThisNumber = process.env.TEXT_THIS_NUMBER;; // Environmental Variable of the number you want to text here. Note it must be verified in https://www.twilio.com/console/phone-numbers/verified
+
+
+// Run Code Every Minute on the Minute
+var rule = new schedule.RecurrenceRule();
+rule.second = 0; // refer to https://github.com/node-schedule/node-schedule for more info
+var j = schedule.scheduleJob(rule, function(){
+
+  // Send Text Message To a (verified) Number
+  var client = new twilio.RestClient(accountSid, authToken);
+  client.messages.create({
+      body: 'Hey there! Another minute has gone by!',
+      to: textThisNumber,  // Text this number
+      from: twilioNumber // From this valid Twilio number
+  }, function(err, message) {
+      if (err) {
+        console.log('Error!')
+        console.log(err);
+      }
+      else {
+        console.log('Message Sent!')
+        console.log(message.sid);
+      }
+  });
+
+  
+});
+
+
+
+
